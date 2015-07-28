b88ddf5081b7a9f0762a8b913bad2e138733090f
Grzegorz Andrejczyn <grzegorz.andrejczyn@allegro.pl>
2015-07-28 20:12:27 +0200

    hystix

--- src/main/java/com/andrejczyn/hystrix/sandbox/runner.java
+++ src/main/java/com/andrejczyn/hystrix/sandbox/runner.java
@@ -1,7 +1,12 @@
 package com.andrejczyn.hystrix.sandbox;
 
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +15,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @EnableAutoConfiguration
 public class Runner {
+
+    private static class Command extends HystrixCommand<String> {
+        protected Command() {
+            super(HystrixCommandGroupKey.Factory.asKey("Test"));
+        }
+
+        @Override
+        protected String run() throws Exception {
+            return "OK";
+        }
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Runner.class, args);
     }
@@ -17,6 +34,12 @@ public class Runner {
     @RequestMapping("/")
     @ResponseBody
     public String command() {
-        return "OK";
+        return new Command().execute();
     }
+
+    @Bean
+    ServletRegistrationBean servletRegistrationBean() {
+        return new ServletRegistrationBean(new HystrixMetricsStreamServlet(), "/hystrix.stream");
+    }
+
 }
